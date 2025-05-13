ed3bf551ec59b6400ddad2846e245c3887f92aee
TravisArnt <tinthtoo008@gmail.com>
2025-05-13 13:33:17 +0700

    Add: add localization file for selfie guide page

--- src/lib/core/l10n/index.ts
+++ src/lib/core/l10n/index.ts
@@ -2,6 +2,7 @@ import type {MultiLingualDictionary} from '@bull-shark/tdh-lib-mason/i18n'
 import {CoreDictionary} from "./core"
 import {IdCardScannerDictionary} from "./id-card-scanner"
 import {PreScreenDictionary} from "./prescreen"
+import { SelfieDictionary } from './selfie'
 
 export enum Language {
   EN = 'en',
@@ -12,4 +13,5 @@ export const Dictionary: MultiLingualDictionary<typeof Language> = {
   core: CoreDictionary,
   idCardScanner: IdCardScannerDictionary,
   preScreen: PreScreenDictionary,
+  selfie:SelfieDictionary
 }
