3c9a9af46758dafca027e153632ef607afb7b300
PatrickSwnn <swannayphueaung0182@gmail.com>
2025-05-13 15:16:55 +0700

    modify(selfie module): preview and capture pages

--- src/lib/module/selfie/view/PreviewCapturedSelfie.svelte
+++ src/lib/module/selfie/view/PreviewCapturedSelfie.svelte
@@ -18,19 +18,38 @@
     HorizontalAlign,
     StickyBottomBar,
     Button,
-    Width
-  } from '@bull-shark/tdh-lib-mason'
-  import LayoutBase from "$lib/module/common/LayoutBase.svelte"
+    Width,
+  } from "@bull-shark/tdh-lib-mason";
+  import LayoutBase from "$lib/module/common/LayoutBase.svelte";
 
-  export let selfieCaptured: string
-  export let onClickNext: () => void
-  export let onClickBack: () => void
-
+  export let selfieCaptured: string;
+  export let onClickNext: () => void;
+  export let onClickBack: () => void;
 </script>
-<LayoutBase titleCaption="ยืนยันตัวตน"
-            footerPrimaryBtnText="ถ่ายใหม่อีกครั้ง"
-            footerSecondaryBtnText="ยืนยัน"
-            onPrimaryBtnClick={onClickNext}
-            onSecondaryBtnClick={onClickBack}>
-    <img src={selfieCaptured}/>
+
+<TopNavBar
+  id="top-nav-bar"
+  title="ยืนยันตัวตน"
+  leftIconName={IconName.West}
+  onLeftIconClick={() => {
+    history.back();
+  }}
+/>
+<LayoutBase
+  titleCaption="ยืนยันรูปคู่บัตรประชาชน"
+  isNeedStickyBottomBar={true}
+  footerPrimaryBtnText="ถ่ายใหม่อีกครั้ง"
+  footerSecondaryBtnText="ยืนยัน"
+  onPrimaryBtnClick={onClickNext}
+  onSecondaryBtnClick={onClickBack}
+>
+  <img
+    src={selfieCaptured}
+    id="selie-preview"
+    class="h-[350px] object-cover selfie-preview"
+    alt=""
+  />
 </LayoutBase>
+
+<style>
+</style>
