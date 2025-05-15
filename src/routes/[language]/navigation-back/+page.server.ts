e1617c892439dec58f57bc4a79a19ea769f8a8f1
popsirawitm1 <popsirawit0945813503@gmail.com>
2025-05-15 14:32:22 +0700

    welcome and navigate-back page ui

---
+++ src/routes/[language]/navigation-back/+page.server.ts
@@ -0,0 +1,11 @@
+// @ts-nocheck
+import type {EntryGenerator} from './$types'
+import {Language} from '$lib/core/l10n'
+
+export const entries: EntryGenerator = () => {
+    return Object
+        .values(Language)
+        .map(l => {
+            return {language: l}
+        })
+}
